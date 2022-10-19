96073959814f0ada44c231a4e9019b1028e29a84
Anders Jefsen <andersjeff397@gmail.com>
2022-10-19 17:17:27 +0200

    tilføjelse af do while loop, så man får ekstra tur, når man lander på feltet 10

--- src/Spil2.java
+++ src/Spil2.java
@@ -18,80 +18,85 @@ public class Spil2 {
         acct2.getBalance();
 
 while((acct1.getBalance()< 3000)&&(acct2.getBalance()<3000) ) {
-    die1pl1.roll();
-    die2pl1.roll();
 
-    System.out.println();
-    System.out.println("Spiller1 slår");
-    System.out.println("Die One: " + die1pl1 + ", Die Two: " + die2pl1);
+    do {
+        die1pl1.roll();
+        die2pl1.roll();
 
-
-    sum1 = die1pl1.getFaceValue() + die2pl1.getFaceValue();
-    System.out.println("Sum: " + sum1);
+        System.out.println();
+        System.out.println("Spiller1 slår");
+        System.out.println("Die One: " + die1pl1 + ", Die Two: " + die2pl1);
 
 
-    if (sum1 == 2) {
-        double P1Balance = acct1.deposit(250);
-        System.out.println("P1 after deposit: " +
-                P1Balance);
-    }
-
-    if (sum1 == 3) {
-        System.out.println("P1 after withdraw " +
-                acct1.withdraw(100));
-    }
-    if (sum1 == 4) {
-        double P1Balance = acct1.deposit(100);
-        System.out.println("P1 after deposit: " +
-                P1Balance);
-    }
-    if (sum1 == 5) {
-        System.out.println("P1 after withdraw " +
-                acct1.withdraw(20));
-    }
-    if (sum1 == 6) {
-        double P1Balance = acct1.deposit(180);
-        System.out.println("P1 after deposit: " +
-                P1Balance);
-    }
-    if (sum1 == 7) {
-        double P1Balance = acct1.deposit(0);
-        System.out.println("P1 after deposit: " +
-                P1Balance);
-
-    }
-    if (sum1 == 8) {
-        System.out.println("P1 after withdraw " +
-                acct1.withdraw(70));
-    }
-    if (sum1 == 9) {
-        double P1Balance = acct1.deposit(60);
-        System.out.println("P1 after deposit: " +
-                P1Balance);
-    }
-    if (sum1 == 10) {
-        System.out.println("P1 after withdraw " +
-                acct1.withdraw(80));
-    }
-    if (sum1 == 11) {
-        System.out.println("P1 after withdraw " +
-                acct1.withdraw(50));
-    }
-    if (sum1 == 12) {
-        double P1Balance = acct1.deposit(650);
-        System.out.println("P1 after deposit: " +
-                P1Balance);
-    }
+        sum1 = die1pl1.getFaceValue() + die2pl1.getFaceValue();
+        System.out.println("Sum: " + sum1);
 
 
-    System.out.println();
-    System.out.println(acct1);
-    System.out.println(acct2);
+        if (sum1 == 2) {
+            double P1Balance = acct1.deposit(250);
+            System.out.println("P1 after deposit: " +
+                    P1Balance);
+        }
+
+        if (sum1 == 3) {
+            System.out.println("P1 after withdraw " +
+                    acct1.withdraw(100));
+        }
+        if (sum1 == 4) {
+            double P1Balance = acct1.deposit(100);
+            System.out.println("P1 after deposit: " +
+                    P1Balance);
+        }
+        if (sum1 == 5) {
+            System.out.println("P1 after withdraw " +
+                    acct1.withdraw(20));
+        }
+        if (sum1 == 6) {
+            double P1Balance = acct1.deposit(180);
+            System.out.println("P1 after deposit: " +
+                    P1Balance);
+        }
+        if (sum1 == 7) {
+            double P1Balance = acct1.deposit(0);
+            System.out.println("P1 after deposit: " +
+                    P1Balance);
+
+        }
+        if (sum1 == 8) {
+            System.out.println("P1 after withdraw " +
+                    acct1.withdraw(70));
+        }
+        if (sum1 == 9) {
+            double P1Balance = acct1.deposit(60);
+            System.out.println("P1 after deposit: " +
+                    P1Balance);
+        }
+        if (sum1 == 10) {
+            System.out.println("P1 after withdraw " +
+                    acct1.withdraw(80));
+
+        }
+        if (sum1 == 11) {
+            System.out.println("P1 after withdraw " +
+                    acct1.withdraw(50));
+        }
+        if (sum1 == 12) {
+            double P1Balance = acct1.deposit(650);
+            System.out.println("P1 after deposit: " +
+                    P1Balance);
+        }
+
+
+        System.out.println();
+        System.out.println(acct1);
+        System.out.println(acct2);
+    }
+while (sum1==10);
 
 
     Die die1pl2, die2pl2;
     int sum2;
-
+    do {
 
     die1pl2 = new Die();
     die2pl2 = new Die();
@@ -107,7 +112,6 @@ while((acct1.getBalance()< 3000)&&(acct2.getBalance()<3000) ) {
 
     sum2 = die1pl2.getFaceValue() + die2pl2.getFaceValue();
     System.out.println("Sum: " + sum2);
-
 
 
     if (sum2 == 2) {
@@ -167,6 +171,8 @@ while((acct1.getBalance()< 3000)&&(acct2.getBalance()<3000) ) {
     System.out.println();
     System.out.println(acct1);
     System.out.println(acct2);
+}
+while(sum2==10);
 }
 }
 }
